738c21aea40a8a7727ffe1469c4339b86833b27a
Udit Kumar Tiwari <159471583+Udit004@users.noreply.github.com>
2026-02-01 15:11:03 +0530

    checking github action

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, Udit!");
+  res.send("Hello, Satyam!");
 });
 
 app.get("/about", (req, res) => {
