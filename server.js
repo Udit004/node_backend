c4855f2c5f3ad20d4dd763a94f9161de33021616
Udit Kumar Tiwari <159471583+Udit004@users.noreply.github.com>
2026-02-01 12:57:55 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express"
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/about", (req, res) => {
+  res.send("This is the about page.");
+});
+
+app.listen(PORT,"0.0.0.0", () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
