7216fb9cd68dcca477b4df4b66680e97716713eb
Udit Kumar Tiwari <159471583+Udit004@users.noreply.github.com>
2026-02-01 17:28:55 +0530

    update name with didi and priya

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, Udit!");
+  res.send("Hello, Didi and Priya!");
 });
 
 app.get("/about", (req, res) => {
